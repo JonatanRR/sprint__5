b16d9afdc89aa94fcad14609959b7f6f88353c0d
Jonatan <jonatan.ramr@gmail.com>
2022-01-08 17:13:40 +0100

    Ejercicio3

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 //Exercise 1 y 2
 document.querySelector('#buttonJoke').addEventListener('click', getData);
 var contentJoke = document.querySelector('#contentJoke');
+var vote = document.querySelector('#vote');
 var petition = {
     method: 'GET',
     headers: {
@@ -13,7 +14,21 @@ function getData() {
         .then(function (data) {
         contentJoke.innerText = data.joke;
         //console.log(data.joke);
+        vote.innerHTML = "<button type=\"button\" onclick=\"getVote(1)\">1</button>\n        <button type=\"button\" onclick=\"getVote(2)\">2</button>\n        <button type=\"button\" onclick=\"getVote(3)\">3</button>";
     })["catch"](function (error) {
         console.log('Ha ocurrido un error', error);
     });
+}
+//Exercise 3 (Lines: 4, 18, 19 & 20, are added as a part of this exercise)
+var reportJokes = Array();
+function getVote(id) {
+    var d = new Date();
+    var text = d.toISOString();
+    var properties = {
+        "joke": contentJoke.innerText,
+        "score": id,
+        "date": text
+    };
+    console.log(reportJokes);
+    reportJokes.push(properties);
 }
--- index.ts
+++ index.ts
@@ -1,6 +1,7 @@
 //Exercise 1 y 2
 document.querySelector('#buttonJoke').addEventListener('click', getData);
 const contentJoke = <HTMLElement>document.querySelector('#contentJoke');
+const vote = <HTMLElement>document.querySelector('#vote');
 const petition = {
    method: 'GET',
    headers: {
@@ -14,11 +15,26 @@ function getData(){
     .then(data => {
         contentJoke.innerText = data.joke;
         //console.log(data.joke);
+        vote.innerHTML = `<button type="button" onclick="getVote(1)">1</button>
+        <button type="button" onclick="getVote(2)">2</button>
+        <button type="button" onclick="getVote(3)">3</button>`
     })
     .catch(error => {
         console.log('Ha ocurrido un error', error);
     });
 }
 
-
+//Exercise 3 (Lines: 4, 18, 19 & 20, are added as a part of this exercise)
+let reportJokes= Array<Object>;
+function getVote(id: number) {
+    const d = new Date();
+    let text= d.toISOString();
+    let properties= {
+        "joke" : contentJoke.innerText,
+        "score" : id,
+        "date": text,
+    };
+    console.log(reportJokes);
+    reportJokes.push(properties);
+}
 
